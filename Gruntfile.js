bdb30ebddd9c53b015979c4478793c569b2e45b0
Pravin Jadhav <pravinjadhav762@gmail.com>
2017-05-03 17:11:57 +0530

    changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,23 +5,40 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
 
-    sass: {
-    dist: {
-      files: [{
-        expand: true,
-        cwd: 'client',
-        src: ['scss/*.scss'],
-        dest: 'client/css',
-        ext: '.css'
-      }]
-    }
-  }
+          sass: {
+          dist: {
+            files: [{
+              expand: true,
+              cwd: 'client',
+              src: ['scss/*.scss'],
+              dest: 'client/css',
+              ext: '.css'
+            }]
+          }
+        },
+        compass: {
+                files: [
+                  'client/scss/*.scss',
+                ],
+                tasks: ['compass']
+              },
+        watch: {
+            css: {
+                files: 'client/scss/*.scss',
+
+                tasks: ['sass']
+              }          
+        },
   });
 
   // Load the plugin that provides the "uglify" task.
  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-express-server');
   // Default task(s).
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['sass','watch','compass','server']);
 
 };
