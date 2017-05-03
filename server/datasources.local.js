ac4f370c79e862b6b608edb7448939c5a9411cda
Pravin Jadhav <pravinjadhav762@gmail.com>
2017-05-03 17:44:17 +0530

    changes

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,6 +1,10 @@
 module.exports = {
 mongodb: {
-url: process.env.DB_URL || 'mongodb://localhost:27017/musicDB',
+url:'mongodb://admin:admin@ds129651.mlab.com:29651/musicapp',
 connector: process.env.CONNECTOR || 'loopback-connector-mongodb' //https://docs.strongloop.com/display/public/LB/MongoDB+connector#MongoDBconnector-Replicasetconfiguration
 }
 };
+
+//mongodb://<dbuser>:<dbpassword>@ds129651.mlab.com:29651/musicapp
+
+//mongodb://localhost:27017/musicDB
